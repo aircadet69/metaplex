46c8c904a521f35aea23bdb357799e450d38ac2f
aircadet69 <45757965+aircadet69@users.noreply.github.com>
2022-03-10 17:28:40 +0000

    Update index.tsx

--- js/packages/web/src/views/home/components/SalesList/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/index.tsx
@@ -1,7 +1,8 @@
 import { useWallet } from '@solana/wallet-adapter-react';
 import { Col, Layout, Row, Tabs } from 'antd';
 import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
+import useSWR from "swr";
 
 import { useMeta } from '../../../../contexts';
 import { CardLoader } from '../../../../components/MyLoader';
@@ -13,6 +14,50 @@ import { AuctionRenderCard } from '../../../../components/AuctionRenderCard';
 
 const { TabPane } = Tabs;
 const { Content } = Layout;
+
+
+const feed2 = "https://kreationcms.bubbleapps.io/version-test/api/1.1/obj/KreationUser?&constraints=%5B%7B%22key%22%3A%22kreationuseridv2%22%2C%22constraint_type%22%3A%22text%20contains%22%2C%20%22value%22%3A%22"+id+"%22%7D%5D";
+
+  console.log( feed2 )
+
+  const RenderOwnedList = ({ feed }: { feed: string }) => {
+  const { data, error } = useSWR( feed2,
+    (url: string) => fetch(url).then((res) => res.json())
+  );
+
+
+
+  console.log( data )
+
+  if (!data) {
+    // loading
+    return <Fragment />;
+  }
+  if (error) {
+    // error
+    return <Fragment />;
+  }
+
+  return data.response.results.map((kuser: any) => {
+
+   console.log( kuser )
+
+    return (
+ 
+            <div className="profilewrap">
+            <div className="profilecover"><img src={kuser.CoverPicture}/></div>
+            <div className="profilemain">
+        <div className="profileitem profilepicture"><img src={kuser.ProfilePicture}/></div>
+         <div className="profileitem username">{kuser.Username} <div className="verifiedtick"><div className={kuser.Verified}></div></div></div>
+         <div className="profileitem biography">{kuser.Bio}</div>
+         <div className="sociallink twitter"><a href={kuser.Twitter}>{kuser.Twitter}</a></div>
+         <div className="sociallink instagram"><a href={kuser.Instagram}>{kuser.Instagram}</a></div>
+         <div className="sociallink website"><a href={kuser.Website}>{kuser.Website}</a></div>
+         </div>
+</div>
+
+
+    );
 
 export enum LiveAuctionViewState {
   All = '0',
@@ -51,6 +96,9 @@ export const SalesListView = () => {
           </Col>
         </Content>
       </Layout>
+      <div className="feature-list">
+  <RenderOwnedList feed="https://kreationcms.bubbleapps.io/version-test/api/1.1/obj/KreationUser?&constraints=%5B%7B%22key%22%3A%22kreationuseridv2%22%2C%22constraint_type%22%3A%22text%20contains%22%2C%20%22value%22%3A%22${creator?.info.address}%22%7D%5D"/>
+            </div>
     </>
   );
 };
