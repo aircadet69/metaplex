c0408b488ff98f45481cd661d900922fae780d3d
aircadet69 <45757965+aircadet69@users.noreply.github.com>
2022-03-11 00:04:32 +0000

    Update index.tsx

--- js/packages/web/src/views/creatorsv2/index.tsx
+++ js/packages/web/src/views/creatorsv2/index.tsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import React, { useState, Fragment } from 'react';
 import useSWR from "swr";
 
-import { AuctionRenderCard } from '../../../../components/AuctionRenderCard';
 
 
 
